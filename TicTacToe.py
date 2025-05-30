994166ad489ce48694502b3016d7e66506564acb
Olawale Peace <olawalepeace0@gmail.com>
2025-05-30 02:08:53 +0100

    Refactored code

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,30 +3,25 @@ class TicTacToe:
         self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         self.players = {"Player1": 'X', "Player2": 'O'}
         self.active_player = "Player1"
-        self.turn = 1
+        self.turn = 0
 
     def play_move(self, row_index, column_index):
+        self.increment_turn_count()
         active_player = self.get_active_player()
         board = self.get_board()
-        player_symbol = self.get_player_symbol(active_player)
+        player_symbol = self.return_player_symbol(active_player)
 
         if self.is_move_valid(row_index, column_index):
             board[row_index][column_index] = player_symbol
-            print(f"Remarks:{self.check_end_of_game(active_player)}")
             self.next_game_turn()
         else:
             raise Exception("Invalid Move")
 
     def next_game_turn(self):
         active_player = self.active_player
-        self.set_next_turn()
         turn = self.get_current_turn()
-        if turn<=9:
-            if active_player == "Player2":
-                new_active_player = "Player1"
-            else:
-                new_active_player = "Player2"
-
+        if turn < 9:
+            new_active_player = 'Player2' if active_player == 'Player1' else 'Player1'
             self.set_active_player(new_active_player)
         return
 
@@ -38,63 +33,71 @@ class TicTacToe:
         print("\n")
         return
 
-    def show_active_player(self):
+    def print_active_player(self):
         print(self.active_player)
         return
 
-    def check_end_of_game(self, player):
+    def check_end_of_game(self):
         # Game ends when a player wins or turn is 9
-        player_wins = False
-        player_symbol = self.players.get(player)
-
-        # This logic checks for horizontal and vertical wins on  the board
-        for i in range(3):
-            if ((self.board[i][0] == self.board[i][1] == self.board[i][2] == player_symbol)
-                    or (self.board[0][i] == self.board[1][i] == self.board[2][i] == player_symbol)):
-                player_wins = True
-                break
+        there_is_a_winner = False
+        turn = self.get_current_turn()
+        players = self.get_players()
 
         # This logic checks for diagonal wins on the board
-        if ((self.board[0][0] == self.board[1][1] == self.board[2][2] == player_symbol)
-                or (self.board[0][2] == self.board[1][1] == self.board[2][0] == player_symbol)):
-            player_wins = True
+        if ((self.board[0][0] == self.board[1][1] == self.board[2][2])
+                or (self.board[0][2] == self.board[1][1] == self.board[2][0])):
+            there_is_a_winner = True
+            player_symbol = self.board[1][1]
+        else:
+            # This logic checks for horizontal and vertical wins on  the board
+            for i in range(3):
+                if self.board[i][0] == self.board[i][1] == self.board[i][2]:
+                    there_is_a_winner = True
+                    player_symbol = self.board[i][0]
+                    break
 
-        if player_wins:
-            return f"{player} wins"
-        elif self.turn == 9:
-            return f"Draw"
+                elif self.board[0][i] == self.board[1][i] == self.board[2][i]:
+                    there_is_a_winner = True
+                    player_symbol = self.board[0][i]
+                    break
+
+        if there_is_a_winner:
+            for player_name, character in players.items():
+                if character == player_symbol:
+                    return f"{player_name} wins"
+        elif turn == 9:
+            return "Draw"
         return
+
+    def determine_cell_occupant(self, cell_index):
+        board = self.get_board()
+        x, y = cell_index
+        return board[x][y]
 
     def set_active_player(self, new_active_player):
         self.active_player = new_active_player
         return
 
-    def set_next_turn(self):
-        turn =self.turn
-        turn += 1
-        self.turn = turn
+    def increment_turn_count(self):
+        self.turn += 1
         return
 
     def get_active_player(self):
-        active_player = self.active_player
-        return active_player
+        return self.active_player
 
     def get_players(self):
-        players = self.players
-        return players
+        return self.players
 
     def get_board(self):
-        board = self.board
-        return board
+        return self.board
 
-    def get_player_symbol(self, active_player):
+    def return_player_symbol(self, active_player):
         players = self.get_players()
         player_symbol = players.get(active_player)
         return player_symbol
 
     def get_current_turn(self):
-        turn = self.turn
-        return turn
+        return self.turn
 
     def is_move_valid(self, row, column):
         cell_exists = True
